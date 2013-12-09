9f195c55e7fa4a47181bc1f99e850f15295b4536
Nhut Huynh <Ne7ven@yahoo.com>
2013-12-10 00:54:42 +0700

    update slide show, price in detail.html

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 ﻿$(document).bind('ready', function () {
     $('[data-ride="carousel"]').each(function () {
         var $carousel = $(this);
-        console.log($carousel.data());
         $carousel.carousel($carousel.data());
+    });
+
+    $('.pro-img-link').bind('click', function () {
+        var href = $(this).attr('href');
+        if ($(href).length == 0) return;
+        else $(href).css('opacity', 1).siblings().css('opacity', 0);
+
+        return false;
     });
 
 });
